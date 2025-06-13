4e010b044559d786dbe51eda68e55cabdfb87d1b
MDevHub <musbaudeenabdulrahman040@gmail.com>
2025-06-13 02:24:45 +0100

    Added vercel.json to handle all pages correctly

--- src/Components/HomePage/Section1.jsx
+++ src/Components/HomePage/Section1.jsx
@@ -104,7 +104,7 @@
 				</div>
 
 				{/* Right Image */}
-				<div className="h-screen top-2 flex-1 hidden  md:flex justify-center items-end relative">
+				<div className="h-screen md:top-2 flex-1 hidden  md:flex justify-center items-end relative">
 					<img src={img} alt={foodName} className="h-[70%] object-contain" />
 	
 				</div>
